33694b32aa87972be14e28168b2d4d88a563b133
KrissGaliano <kriss.galiano@tecup.edu.pe>
2022-04-16 21:20:34 -0500

    Agregar archivos de proyecto.

---
+++ WpfApp1/MainWindow.xaml.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace WpfApp1
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        SqlConnection connection = new SqlConnection(@"data source=DESKTOP-3627FO8\SQLEXPRESS; initial catalog = db_lab03;  User Id=UserKG ; Password=123456");
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e) 
+        {
+            //Forma Conectada Procedimiento Almacenado
+            List<Person> people = new List<Person>();
+
+            connection.Open();
+            SqlCommand command = new SqlCommand("BuscarPersonaNombre", connection);
+            command.CommandType = System.Data.CommandType.StoredProcedure;
+
+            SqlParameter parameter1 = new SqlParameter();
+            parameter1.SqlDbType = SqlDbType.VarChar;
+            parameter1.Size = 50;
+            //paremeter1.Value = txtLastName.Text.Trim();
+            parameter1.Value = "";
+            parameter1.ParameterName = "@LastName";
+
+            SqlParameter parameter2 = new SqlParameter();
+            parameter2.SqlDbType = SqlDbType.VarChar;
+            parameter2.Size = 50;
+            //paremeter2.Value = txtFirstName.Text.Trim();
+            parameter2.Value = "";
+            parameter2.ParameterName = "@FirstName";
+
+            command.Parameters.Add(parameter1);
+            command.Parameters.Add(parameter2);
+
+            SqlDataReader dataReader = command.ExecuteReader();
+
+            while (dataReader.Read()) 
+            {
+                people.Add(new Person
+                {
+                    PersonID = dataReader["PeopleID"].ToString(),
+                    LastName = dataReader["LastName"].ToString(),
+                    FirstName = dataReader["FirstName"].ToString(),
+                    EnrollmentDate = dataReader["Date"].ToString()
+                });
+            }
+
+            connection.Close();
+            dvgPeople.ItemsSource = people;
+
+        }
+
+        private void btnNuevo_Click(object sender, RoutedEventArgs e)
+        { 
+            
+        }
+    }
+}
